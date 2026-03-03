2f8eee2ad52ff288c3672e27b7459ab3dc018532
natalie-esuha <natalie.esuha@strathmore.edu>
2026-03-03 16:45:19 +0300

    first

--- shop/myapp/forms.py
+++ shop/myapp/forms.py
@@ -2,4 +2,12 @@ from . models import Product
 from django import forms
 
 class ProductForm():
-    
+    class Meta:
+        model = Product
+        fields = {'name','description', 'price','image'}
+        widgets={
+            'name':forms.TextInput(attrs={'class':'form-control'}),
+            'description':forms.Textarea(attrs={'class':'form-control'}),
+            'price':forms. NumberInput(attrs={'class':'form-control'}),
+            'image':forms.FileInput(attrs={'class':'form-control'}),
+        }
--- shop/myapp/views.py
+++ shop/myapp/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render,redirect
-
+from  .forms import ProductForm
 # Create your views here.
 def index(request):
     return render(request,'index.html')
 
 def add_product(request):
   if request.method =='POST':
-    form=ProductForm()
+    form= ProductForm()
     if form.is_valid():
         form.save()
         return redirect('index')
-    return render(request,'index.html')
+    return render(request,'index.html') 
